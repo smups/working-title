ee2bfedcd71424a441a61a72f424ed2c6a1e155b
Raúl Wolters <56522208+smups@users.noreply.github.com>
2022-05-28 14:50:09 +0200

    changed license from GPL to EUPL

--- srvr/src/client/net.rs
+++ srvr/src/client/net.rs
@@ -3,18 +3,23 @@
   
   This file is part of srvr.
   
-  srvr is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
+  srvr is free software: you can redistribute it and/or modify it under the
+  terms of the European Union Public License (EUPL), provided that you publish
+  your modifications under the terms of the EUPL or another compatible license
+  as specified by the EUPL v1.2 or higher.
+
+  As the copyright holder is a citizen of the Kingdom of the Netherlands, this
+  license agreement shall be governed by dutch law, as specified in clause 17
+  of the EUPL v1.2.
+
+  srvr is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+  A PARTICULAR PURPOSE.  See the European Union Public License for more details.
   
-  srvr is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-  
-  You should have received a copy of the GNU General Public License
-  along with srvr.  If not, see <http://www.gnu.org/licenses/>.
+  You should have received a copy of the European Union Public License in a
+  official language of the European Union along with srvr. If not, see
+  <https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12> for the full
+  text of the license in any official language of the European Union.
 */
 
 use std::net::TcpStream;
